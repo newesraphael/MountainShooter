e4aabf3af658d5b933f1cb283c533e838061a8f8
Raphael Neves <raphaelsrneves@gmail.com>
2026-06-22 20:45:24 -0300

    Level 2 implementation

--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -6,6 +6,7 @@ from code.Player import Player
 import random
 from code.Enemy import Enemy
 
+
 class EntityFactory:
 
     @staticmethod
@@ -13,12 +14,18 @@ class EntityFactory:
         match entity_name:
             case 'Level1Bg':
                 list_bg = []
-                for i in range(7):
+                for i in range(7):  # level1bg images number
                     list_bg.append(Background(f'Level1Bg{i}', (0, 0)))
                     list_bg.append(Background(f'Level1Bg{i}', (WIN_WIDTH, 0)))
                 return list_bg
+            case 'Level2Bg':
+                list_bg = []
+                for i in range(5):  #  level2bg images number
+                    list_bg.append(Background(f'Level2Bg{i}', (0, 0)))
+                    list_bg.append(Background(f'Level2Bg{i}', (WIN_WIDTH, 0)))
+                return list_bg
             case 'Player1':
-                return Player('Player1',(10, WIN_HEIGHT / 2 - 30))
+                return Player('Player1', (10, WIN_HEIGHT / 2 - 30))
             case 'Player2':
                 return Player('Player2', (10, WIN_HEIGHT / 2 + 30))
             case 'Enemy1':
